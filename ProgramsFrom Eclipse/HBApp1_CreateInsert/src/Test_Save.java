1453cb2a3f3730ba1ed409db8ca35d66f3b1e140
chantigit <chantifacts1@gmail.com>
2021-09-22 11:13:18 +0530

    HB Session 3

---
+++ ProgramsFrom Eclipse/HBApp1_CreateInsert/src/Test_Save.java
@@ -0,0 +1,33 @@
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Test_Save {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg=new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		Employee employee=new Employee();
+		employee.setEmployeeId(501);
+		employee.setEmployeeName("Ajay");
+		employee.setEmployeeSalary(45000);
+		
+		Serializable result = session.save(employee);  //Prepare insert query
+		System.out.println("result="+result);
+		
+		System.out.println("Object saved into DB successfully");
+		tx.commit();
+		session.close();
+		factory.close();
+
+	}
+
+}
