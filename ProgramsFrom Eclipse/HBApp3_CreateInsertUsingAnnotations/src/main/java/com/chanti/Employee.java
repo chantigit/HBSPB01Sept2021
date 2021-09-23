eba03aa5a811201b5240a634226cc70206b3bdf7
chantigit <chantifacts1@gmail.com>
2021-09-23 11:37:11 +0530

    Hb Session4

---
+++ ProgramsFrom Eclipse/HBApp3_CreateInsertUsingAnnotations/src/main/java/com/chanti/Employee.java
@@ -0,0 +1,53 @@
+package com.chanti;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="emp01")
+public class Employee 
+{
+	@Id
+	@Column(name = "emp_id")
+	private int employeeId;
+	@Column(name="emp_name")
+	private String employeeName;
+	@Column(name="emp_salary")
+	private float employeeSalary;
+	
+	public Employee() {
+	}
+	public Employee(int employeeId, String employeeName, float employeeSalary) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public float getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(float employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeSalary="
+				+ employeeSalary + "]";
+	}
+	
+
+}
---
+++ ProgramsFrom Eclipse/HBApp3_CreateInsertUsingAnnotations/src/main/java/com/chanti/Test_Save.java
@@ -0,0 +1,34 @@
+package com.chanti;
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Test_Save {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg=new Configuration();
+		cfg.configure("com\\chanti\\hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		Employee employee=new Employee();
+		employee.setEmployeeId(501);
+		employee.setEmployeeName("Depak");
+		employee.setEmployeeSalary(45000);
+		
+		Serializable result = session.save(employee);  //Prepare insert query
+		System.out.println("result="+result);
+		
+		System.out.println("Object saved into DB successfully");
+		tx.commit();
+		session.close();
+		factory.close();
+
+	}
+
+}
